415f0de5a611c2b2be8de000b067dffbd5a2cc4f
Myron <dubtwee@gmail.com>
2021-01-26 11:10:44 -0600

    adding a list of various penguins

--- client.js
+++ client.js
@@ -34,3 +34,62 @@ const bowlerHat = {
 };
 
 console.log(bowlerHat.interior['color']);
+
+// PENGUINS
+
+// {
+//   name: '',
+//   colors: [''],
+//   size: 1.0,
+//   isCute: true,
+// }
+const penguins = [
+  {
+    name: 'Emperor',
+    colors: ['Black', 'White', 'Yellow'],
+    size: 1.0,
+    isCute: true,
+  },
+  {
+    name: 'Little',
+    colors: ['Slate-blue', 'White'],
+    size: 0.5,
+    isCute: true,
+  },
+  {
+    name: 'Rockhopper',
+    colors: ['Black', 'White', 'Yellow'],
+    size: 0.75,
+    isCute: true,
+  },
+  {
+    name: 'African',
+    colors: ['Black', 'White'],
+    size: 0.5,
+    isCute: true,
+  },
+  {
+    name: 'Gentoo',
+    colors: ['Black', 'White'],
+    size: 0.5,
+    isCute: true,
+  },
+  {
+    name: 'Sea Gull',
+    colors: ['Gray'],
+    size: 0.25,
+    isCute: false,
+  },
+  {
+    name: 'Chinstrap',
+    colors: ['Black', 'White'],
+    size: 0.25,
+    isCute: true,
+  },
+  {
+    name: 'Macaroni',
+    colors: ['Black', 'White', 'Yellow'],
+    size: 0.5,
+    isCute: true,
+  },
+];
