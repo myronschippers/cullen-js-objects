9c913db850fb4473794f0541b11866306e9c6bc6
Myron <dubtwee@gmail.com>
2021-01-26 10:31:24 -0600

    initial objects

--- client.js
+++ client.js
@@ -1,1 +1,24 @@
 console.log('Hello Cullen');
+
+const topHat = {
+  type: 'Top',
+  color: 'black',
+  size: 14,
+};
+
+console.log(topHat.color);
+console.log(topHat.size);
+
+const baseballCap = {
+  type: 'Baseball Cap',
+  color: 'Red and White',
+  size: 12,
+};
+
+const hatColorPropKey = 'color';
+
+console.log(baseballCap.type);
+// for key as string
+console.log(baseballCap[hatColorPropKey]);
+
+console.log(Object.keys(baseballCap));
